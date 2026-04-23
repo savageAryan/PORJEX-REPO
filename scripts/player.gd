4b2363197a5e0406909f41cd288645be222cb57c
Aryan Patel <bontaromiii@gmail.com>
2026-04-23 18:15:06 +0530

    added audio and levelup game

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,30 @@
 extends CharacterBody2D
+@onready var jump_count = 0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jumpaudio: AudioStreamPlayer = $jumpaudio
 
 
-const SPEED = 800.0
+const SPEED = 1000.0
 const JUMP_VELOCITY = -1500.0
 
+@onready var doublejump: AudioStreamPlayer = $doublejump
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta * 3
+		velocity += get_gravity() * delta * 3.3
+		
+	else:
+		jump_count = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_count < 2:
+		
+		jump_count += 1
+	
 		velocity.y = JUMP_VELOCITY 
+		if jump_count >0:
+			jumpaudio.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -32,8 +43,14 @@ func _physics_process(delta: float) -> void:
 			animated_sprite_2d.play("walking")
 			
 	else: 
-		animated_sprite_2d.play("jumping")
+		
+		if jump_count > 1:
+			animated_sprite_2d.play("doube jump")
+			doublejump.play()
 			
+			
+			JUMP_VELOCITY + 50
+		
 	
 	if direction:
 		velocity.x = direction * SPEED
